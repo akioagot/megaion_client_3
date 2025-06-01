b557305f28eea5eb1349f6752f8752988bcab4ba
hymaybituin <hymaybituin@student.apc.edu.ph>
2025-06-01 20:31:43 +0800

    remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/megaion_client_3/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
